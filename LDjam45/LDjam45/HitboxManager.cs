85ba8299e96d3fdc3b3cecb19547a3dac3fea9bc
Nick Pels <nxp2196@rit.edu>
2019-10-04 21:10:44 -0400

    Some errors, idea is done, will fix

--- LDjam45/LDjam45/HitboxManager.cs
+++ LDjam45/LDjam45/HitboxManager.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace LDjam45
 {
     class HitboxManager
     {
+        Rectangle hitBox;
+        List<Rectangle> hitBoxActive;
+        List<Rectangle> hitBoxInactive;
+
+        public void ActivateHitbox(Rectangle hitbox, SpriteBatch spriteBatch, Texture2D texture)
+        {
+            spriteBatch.Draw(texture, Color.White, hitbox);//Fix
+        }
+        public void CheckCollision(Rectangle hitbox,Rectangle cRect,Character cObj)
+        {
+            if (hitbox.IntersectsWith(cRect))
+            {
+                cObj.health -= 1;
+            }
+        }
+        public void DeleteHitbox(Rectangle hitbox)
+        {
+            hitbox.X == -50;
+        }
     }
 }
