6f5cacdb3e0c241fb838b7b4d2a935b0a24df2e8
Nick Pels <nxp2196@rit.edu>
2019-10-06 11:55:55 -0400

    Menu done

--- LDjam45/LDjam45/Game1.cs
+++ LDjam45/LDjam45/Game1.cs
@@ -41,7 +41,13 @@ namespace LDjam45
         //characters
         Character player0;
         Character player1;
-
+        //Menu
+        Rectangle r1;
+        Rectangle r2;
+        Rectangle r3;
+        MouseState mState;
+        bool choice1;
+        bool choice2;
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -60,6 +66,7 @@ namespace LDjam45
         {
             // TODO: Add your initialization logic here
             gState = gameState.game;
+            mState = new MouseState();
             base.Initialize();
         }
 
@@ -112,8 +119,61 @@ namespace LDjam45
                 case gameState.gameOver:
                     break;
                 case gameState.menu:
-                    if(SingleKeyPress(Keys.Enter))
+                    r1 = new Rectangle(100, 100, 100, 100);//Character selection boxes
+                    r2 = new Rectangle(200, 100, 100, 100);
+                    r3 = new Rectangle(300, 100, 100, 100);
+                    if (mState.Y > 100 && mState.Y < 200)//If the mouse is within the y range
                     {
+                        if (mState.X > 100 && mState.X < 200)//If the mouse is within the x ranges
+                        {
+                            if (mState.LeftButton == ButtonState.Pressed)
+                            {
+                                player0 = new Character(playerType.gunPlayer, 0, new Rectangle(100, 100, 100, 100), GunnerSpritesheet);
+                                choice1 = true;//Checks if the players have chosen characters
+                            }
+                            else if (mState.RightButton == ButtonState.Pressed)
+                            {
+                                player1 = new Character(playerType.gunPlayer, 1, new Rectangle(400, 100, 100, 100), GunnerSpritesheet);
+                                choice2 = true;
+                            }
+                        }
+                        else if (mState.X > 200 && mState.X < 300)
+                        {
+                            if (mState.LeftButton == ButtonState.Pressed)
+                            {
+                                //player0 = new Character(playerType.swordPlayer, 0, new Rectangle(100, 100, 100, 100), SwordfighterSpritesheet);
+                                choice1 = true;
+                            }
+                            else if (mState.RightButton == ButtonState.Pressed)
+                            {
+                                //player1 = new Character(playerType.swordPlayer, 1, new Rectangle(400, 100, 100, 100), SwordfighterSpritesheet);
+                                choice2 = true;
+                            }
+                        }
+                        else if (mState.X > 300 && mState.X < 400)
+                        {
+                            if (mState.LeftButton == ButtonState.Pressed)
+                            {
+                                //player0 = new Character(playerType.flailPlayer, 0, new Rectangle(100, 100, 100, 100), GunnerSpritesheet);
+                                choice1 = true;
+                            }
+                            else if (mState.RightButton == ButtonState.Pressed)
+                            {
+                                //player1 = new Character(playerType.flailPlayer, 1, new Rectangle(400, 100, 100, 100), GunnerSpritesheet);
+                                choice2 = true;
+                            }
+                        }
+                    }
+                    if(SingleKeyPress(Keys.Space))
+                    {
+                        if (choice1 != true)//If a character has not been chosen, the player is defaulted to gunner
+                        {
+                            player0 = new Character(playerType.gunPlayer, 0, new Rectangle(100, 100, 100, 100), GunnerSpritesheet);
+                        }
+                        if (choice2 != true)
+                        {
+                            player1 = new Character(playerType.gunPlayer, 1, new Rectangle(400, 100, 100, 100), GunnerSpritesheet);
+                        }
                         gState = gameState.game;
                     }
                     break;
